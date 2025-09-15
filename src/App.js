875a535747d76ce91efc2f5ed9a400d013d3bf73
Karim Keraani <karimkeraani24@gmail.com>
2025-09-15 14:28:46 +0200

    refactor: change code to accomodate new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react' ; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './frontend/components/Navbar';
+import Navbar from './frontend/components/Navbar.js';
+import Home from './frontend/components/Home.js';
 import {LoginForm, RegisterForm} from './frontend/components/Form';
 import './App.css';
+
 
 function App() {
     return ( <>
         <Router>
             <Navbar />
             <Routes>
+            <Route path='/' element = { <Home/>} />
             <Route path='/login' element = { <LoginForm/>} />
             <Route path='/register' element = { <RegisterForm/>} />
             </Routes>
