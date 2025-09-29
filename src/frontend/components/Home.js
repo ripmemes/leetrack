8323ca985d1e7a762edff28f2b8745c21441e1ee
Karim Keraani <karimkeraani24@gmail.com>
2025-09-29 23:35:40 +0200

    feat: add new prop to sync login status between relevant components

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React ,{useState, useEffect} from 'react'
 import {Daily} from './PostCard.js'
 import ContestBar from './ContestBar.js'
 import ProblemLst from './ProblemLst.js'
 import AIComponent from './AIComponent.js';
-// For overview, after authenticating 
-function Home(){
-    // TOP LEFT : Contests
-    // BOTTOM LEFT : Problem list : 10 at a time 
-    // RIGHT : Daily Challenge
+
+function Home({logged,setLogged}){
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        if (!token){
+            return
+        }
+
+        fetch("http://localhost:5000/",{
+            headers:{
+                "Authorization" : "Bearer " + token
+            }
+        })
+        .then(res => {
+            if (!res.ok) throw new Error("Not authorized")
+            return res.json()
+        })
+        .then(data=>setLogged(true))
+        .catch(()=> window.location.href="/login")
+    },[])
+
 
     return (
-    <div className="relative grid grid-cols-2 bg-gray-100 ">
-        <div><ContestBar></ContestBar></div>
-        <div className="h-28"><Daily/></div>
-        <div className="bg-slate-600"><ProblemLst/></div>
-        <div><AIComponent/></div>
-    </div>   
+    <div className="bg-gray-100 min-h-screen flex justify-center">
+        {!logged && <div>PLACEHOLDER PRESENTATION PAGE</div>}
+        { logged && <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full max-w-6xl px-4">
+            <div><ContestBar></ContestBar></div>
+            <div className=""><Daily/></div>
+            <div className="bg-slate-200 border rounded-lg "><ProblemLst/></div>
+            <div><AIComponent/></div>
+        </div>  }
+    </div>  
     )
 }
 
