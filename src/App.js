db7d5c705798088152013c61e7a443a8c1d2641d
Karim Keraani <karimkeraani24@gmail.com>
2025-09-29 23:33:34 +0200

    ref: refactor code to accomdate changes in authentification logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react' ; 
+import React, {useState , useEffect} from 'react' ; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './frontend/components/Navbar.js';
 import Home from './frontend/components/Home.js';
@@ -7,12 +7,18 @@ import './App.css';
 
 
 function App() {
+    const [logged, setLogged] = useState(false)
+    useEffect(()=> {
+        const token = localStorage.getItem("token")
+        if (token) setLogged(true)
+    },[])
+
     return ( <>
         <Router>
-            <Navbar />
+            <Navbar logged={logged} setLogged={setLogged} />
             <Routes>
-            <Route path='/' element = { <Home/>} />
-            <Route path='/login' element = { <LoginForm/>} />
+            <Route path='/' element = { <Home logged={logged} setLogged={setLogged}/>} />
+            <Route path='/login' element = { <LoginForm setLogged={setLogged}/>} />
             <Route path='/register' element = { <RegisterForm/>} />
             </Routes>
         </Router>
